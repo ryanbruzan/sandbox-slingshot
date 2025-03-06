e79787cde38fe7e8178c3684ba6a5fe9d5c10e8c
Ryan Bruzan <ryanbruzan220@gmail.com>
2025-03-06 17:59:43 -0500

    fix: path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/sandbox-slingshot/",
 	plugins: [tsconfigPaths()],
 });
